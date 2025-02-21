63b9b6a79d3d68682f0b049f60dfac48a2af29cf
Devjeetpatel <159103832+Devjeetpatel@users.noreply.github.com>
2025-02-21 10:22:05 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+document.getElementById
+("share").addEventListener("click",()=>{
+    window.location.href="whatsapp://send?text=https://devjeetpatel.github.io/Bookmark-site/";
+})
+
+
+document.getElementById("form").addEventListener("click",()=>{window.location.href='https://forms.gle/rYVuzLj7Uy9uFJ6Y7'})
